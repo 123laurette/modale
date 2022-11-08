a40558557a7e940463ced4e2e5f0cbcea2876cba
laurette Dalmoro <dalmorolaurette@orange.fr>
2022-11-08 15:30:27 +0100

    publication réussie

---
+++ dist/Modale.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line strict
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./Modale.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function Modale(_ref) {
+  var closeModale = _ref.closeModale;
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
+    className: "bcgModale"
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    className: "close",
+    onClick: function onClick() {
+      return closeModale(false);
+    }
+  }, " X "), /*#__PURE__*/_react.default.createElement("h5", null, "The new employee has been successfully registered \uD83D\uDC4F ")));
+}
+;
+var _default = Modale;
+exports.default = _default;

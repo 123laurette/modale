c05d5cb2818b9c0143785e83c971d084d5b87cd1
laurette Dalmoro <dalmorolaurette@orange.fr>
2022-11-14 17:03:39 +0100

    suppression du css automatique(lien dans jsx

--- src/lib/Button.jsx
+++ src/lib/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useState} from "react"
 import Modale from "./Modale"
-import "./Button.css"
 
 function Button() {
     const [modal, setModal] = useState(false);

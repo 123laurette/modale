507e181c1fc20e2e3ee9ac14a2d246ea6bbbe759
laurette Dalmoro <dalmorolaurette@orange.fr>
2022-11-14 16:27:26 +0100

    ajout react sur button

--- dist/Modale.js
+++ dist/Modale.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line strict
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
--- src/lib/Button.jsx
+++ src/lib/Button.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {useState} from "react"
 import Modale from "./Modale"
 import "./Button.css"
